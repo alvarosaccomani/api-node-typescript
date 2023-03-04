5c91e28362876ac9820139bc0956abac6ecd1944
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2023-03-04 11:17:41 -0300

    add '/' route to project

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
